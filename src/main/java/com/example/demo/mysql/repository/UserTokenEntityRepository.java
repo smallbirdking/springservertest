b3447492797f5725da6cfd02b7470624aeac9f8d
wangyu <869084711@qq.com>
2019-10-14 23:55:51 +0200

    temp push

--- src/main/java/com/example/demo/mysql/repository/UserTokenEntityRepository.java
+++ src/main/java/com/example/demo/mysql/repository/UserTokenEntityRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.mysql.entity.UserTokenEntity;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +24,11 @@ public interface UserTokenEntityRepository extends CrudRepository<UserTokenEntit
     @Modifying
     @Query("delete from UserTokenEntity where userId = ?1 and refreshToken = ?2")
     int deleteByUsername(long id, String repositoryToken);
+
+    @Transactional()
+    @Modifying
+    @Query("UPDATE UserTokenEntity u SET u.token = :token WHERE u.userId = :id")
+    int upadte(@Param("id") long id,@Param("token") String token);
 //    @Async
 //     (t.dbname like '%'||?1||'%' or ?1='')
 //    ListenableFuture<UserEntity> findOneByLastname(String lastname);
